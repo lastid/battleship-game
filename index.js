28cb0a09c9a2b7f51a9aa905c4b5d0932a58b5b5
lastid <lastid@gmail.com>
2016-11-08 23:54:46 +0000

    Refactor code

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 {
   const { Ship, GridGenerator, Renderer } = window.battleship
-  const GRID_SIZE = 10
-  const ALLOWED_SHIPS = [
-    Ship.TYPE_BATTLESHIP, Ship.TYPE_DESTROYER, Ship.TYPE_DESTROYER
-  ]
-  const letterACharCode = 'A'.charCodeAt(0)
+
+  const LETTER_A_CODE = 'A'.charCodeAt(0)
 
   // convert grid cell (e.g. {row: 0, col: 6} ) to coordinates (eg. A5)
   const convertToCordinates = (cell) => {
-    return String.fromCharCode(cell.row + letterACharCode) + (cell.col + 1)
+    return String.fromCharCode(cell.row + LETTER_A_CODE) + (cell.col + 1)
   }
 
   // convert coordinates (eg. A5) to grid cell (e.g. {row: 0, col: 6} )
   const convertToCell = (coordinates) => {
     return {
-      row: coordinates.charCodeAt(0) - letterACharCode,
+      row: coordinates.charCodeAt(0) - LETTER_A_CODE,
       col: parseInt(coordinates.charAt(1), 10) - 1
     }
   }
@@ -32,6 +29,7 @@
 
     if (ship) {
       const shipName = ship.getName()
+
       log.push(`${pad} HIT: ${shipName}</span>`)
 
       if (ship.isSunken()) {
@@ -48,45 +46,51 @@
     }
   }
 
+  const addListeners = (gameEl, formEl) => {
+    formEl.addEventListener('submit', e => {
+      e.preventDefault()
+
+      if (grid.isOver()) {
+        return
+      }
+
+      const coordinates = formEl.querySelector('input').value.trim().toUpperCase()
+      const cell = convertToCell(coordinates)
+
+      if (grid.containsCell(cell)) {
+        shootAtGrid(cell)
+      } else {
+        log.push('-- Invalid coordinates: ' + coordinates)
+      }
+
+      renderer.render(grid, log)
+    })
+
+    gameEl.addEventListener('click', e => {
+      const target = e.target
+
+      if (!grid.isOver() && target.classList.contains('grid__cell')) {
+        const cellData = target.getAttribute('data-cell')
+        const [row, col] = cellData.split(',').map(Number)
+        const cell = { row, col }
+
+        shootAtGrid(cell)
+        renderer.render(grid, log)
+      }
+    })
+  }
+
+  const GRID_SIZE = 10
+  const ALLOWED_SHIPS = [
+    Ship.TYPE_BATTLESHIP, Ship.TYPE_DESTROYER, Ship.TYPE_DESTROYER
+  ]
   const grid = GridGenerator.generate(GRID_SIZE, ALLOWED_SHIPS)
   const log = []
-
   const gameEl = document.querySelector('.game')
   const formEl = document.querySelector('form')
-
-  formEl.addEventListener('submit', e => {
-    e.preventDefault()
-
-    if (grid.isOver()) {
-      return
-    }
-
-    const inputValue = formEl.querySelector('input').value.trim().toUpperCase()
-    const cell = convertToCell(inputValue)
-
-    if (grid.containsCell(cell)) {
-      shootAtGrid(cell)
-    } else {
-      log.push('-- Invalid coordinates: ' + inputValue)
-    }
-
-    renderer.render(grid, log)
-  })
-
-  gameEl.addEventListener('click', e => {
-    const target = e.target
-
-    if (!grid.isOver() && target.classList.contains('grid__cell')) {
-      const cellData = target.getAttribute('data-cell')
-      const [row, col] = cellData.split(',').map(Number)
-      const cell = { row, col }
-
-      shootAtGrid(cell)
-      renderer.render(grid, log)
-    }
-  })
-
   const renderer = new Renderer(gameEl)
+
+  addListeners(gameEl, formEl)
 
   renderer.render(grid, log)
 }
